6f7449b2aa111c28713ce479965c09bcdf5f0b8e
WannJJ <76657794+WannJJ@users.noreply.github.com>
2025-02-04 17:53:38 +0100

    Update setup.js

--- src/setup.js
+++ src/setup.js
@@ -1048,8 +1048,6 @@ function getClassFromName(class_name) {
     return;
   }
 
-  console.log(all_cards.length);
-
   for (let i = 0; i < all_cards.length; i++) {
     let card_class = all_cards[i];
     if (i < 10) console.log(card_class.name, card_class, class_name);
